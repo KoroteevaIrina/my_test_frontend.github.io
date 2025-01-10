217fe957e174f45ea41f62f49ae87abea9569012
KoroteevaIrina <koroteevais94@yandex.ru>
2025-01-10 20:11:47 +0300

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-document.querySelector('.firstButton').addEventListener('click', function() {
-    const inputString = document.querySelector('.inputString').value;
-    let result = encodeString(inputString);
-    document.querySelector('.result').innerText = result;
+document.querySelector('.firstButton').addEventListener('click', function () {
+  const inputString = document.querySelector('.inputString').value;
+
+  if (!/^[A-Z]*$/.test(inputString)) {
+    alert('Введи только заглавные латинские буквы.');
+    return;
+  }
+
+  let result = encodeString(inputString);
+  document.querySelector('.result').innerText = result;
 });
 
-
 function encodeString(str) {
-    let encoded = '';
-    let count = 1;
+  let encoded = '';
+  let count = 1;
 
-    for (let i = 0; i < str.length; i++) {
-        // Проверяем, является ли текущий символ равным следующему
-        if (str[i] === str[i + 1]) {
-            count++;
-        } else {
-            // Если символ встречается больше одного раза, добавляем счетчик
-            if (count > 1) {
-                encoded += count + str[i];
-            } else {
-                encoded += str[i];
-            }
-            count = 1; // Сбрасываем счетчик
-        }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        encoded += count + str[i];
+      } else {
+        encoded += str[i];
+      }
+      count = 1;
     }
-    return encoded;
+  }
+  return encoded;
 }
 
-document.querySelector('.secondButton').addEventListener('click', function() {
-    const resultText = document.querySelector('.result').innerText;
-    navigator.clipboard.writeText(resultText);
+document.querySelector('.secondButton').addEventListener('click', function () {
+  const resultText = document.querySelector('.result').innerText;
+  navigator.clipboard.writeText(resultText);
 });
